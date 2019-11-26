7488286578428dc7909376717fed4e3063533eb4
andreammgcol <amendez72@misena.edu.co>
2019-11-26 18:54:59 -0500

    addition function duplicate string

--- shell.h
+++ shell.h
@@ -1,8 +1,11 @@
 #ifndef _SHELL_
 #define _SHELL_
 
+#include <stdlib.h>
+
 char *_strcat(char *dest, char *src);
 int _strlen(const char *s);
 int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
 
 #endif
