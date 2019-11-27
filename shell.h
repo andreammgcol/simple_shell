53d7660398318774ce391afa1b74136fd60df098
andreammgcol <amendez72@misena.edu.co>
2019-11-26 19:05:20 -0500

    add _putchar and _puts functions

--- shell.h
+++ shell.h
@@ -2,11 +2,14 @@
 #define _SHELL_
 
 #include <stdlib.h>
+#include <unistd.h>
 
 char *_strcat(char *dest, char *src);
 int _strlen(const char *s);
 int _strcmp(char *s1, char *s2);
 char *_strdup(char *str);
 int _atoi(char *s);
+int _putchar(char c);
+void _puts(char *str);
 
 #endif
